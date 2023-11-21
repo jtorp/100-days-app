b5a30e0362b0db4a477f4ded53608485ae83717c
Christian Torp <torp@Christians-Air.home>
2023-11-21 23:18:38 +0100

    day1 - updates

--- days/day1/day1.js
+++ days/day1/day1.js
@@ -12,6 +12,11 @@ let geoCoordinates = null;
 const town = document.getElementById('town');
 
 
+// let lat =55.765478;
+// let lon= 12.500029
+// let q=`${lat},${lon}`;
+
+
 // Geolocation functions
 function getGeoLocation() {
     location.innerText = 'Locating...';
@@ -49,19 +54,20 @@ if (!navigator.geolocation) {
 }
     
 document.querySelector("#geoLocationBtn").addEventListener("click", getGeoLocation);
-});
-
 
 // ---------------
 // Weather API https://www.weatherapi.com/
 // ---------------
-	
+const currentForcast = document.getElementById('forecast');
+const futureForecast = document.getElementById('future');
+
 let store = {
-        name: 'Kongens Lyngby',
-        last_updated: "2023-11-20 20:00",
-        last_updated_epoch: 1700506800,
+        name: 'Copenhagen',
+        last_updated: "",
+        last_updated_epoch: 0,
         cloud:0,
         conditionText: '',
+        icon:'',
         feelslike_c:0,
         feelslike_f:0,
         is_day:1,
@@ -73,53 +79,47 @@ let store = {
         wind_kph:0, 
         wind_mph:0,
         uv:null //Measure of the strength of the ultraviolet radiation from the sun. May be NULL.
-
-    
 };
 
 
-
-
-let lat =55.765478;
-let lon= 12.500029
-let q=`${lat},${lon}`;
-
-async function getGeoLocationKey() {
+const fetchWeatherData = async () => {
     try {
-        const geopositionKey = await fetch(fetchGeoLocationKey);
-        const geopositionKeyData = await geopositionKey.json();
-       return geopositionKeyData.Key
-    } catch (error) {
-        console.error('Error fetching geoposition data:', error);
-        throw error; 
-    }
-}
-let globalKey;
-
-async function setGlobalKey() {
-    try {
-        globalKey = await getGeoLocationKey();
-        console.log('Global key:', globalKey); // Use the key retrieved here
-    } catch (error) {
-        console.error('Error setting global key:', error);
-        throw error;}
-}
-
-
-
-
-(async () => {
-    try {
-        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=87a5d91b5b6d49aeb31181805232011&q=${store.name}&aqi=no
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=87a5d91b5b6d49aeb31181805232011&q=${store.name}&days=3&aqi=no&alerts=no
         `);
         const weatherData = await response.json();
-        console.log('Weather data:', weatherData);
-        const {location:{name}, last_updated,last_updated_epoch,current:{cloud, feelslike_c, feelslike_f, is_day, temp_c, temp_f, humidity, vis_km, vis_miles, wind_kph, wind_mph, uv, condition:{text:conditionText}}} = weatherData;
+        console.log(weatherData)
+
+        const {
+            location: { name, country, localtime },
+            last_updated,
+            last_updated_epoch,
+            current: {
+              cloud,
+              feelslike_c,
+              feelslike_f,
+              is_day,
+              temp_c,
+              temp_f,
+              humidity,
+              vis_km,
+              vis_miles,
+              wind_kph,
+              gust_kph,
+              wind_mph,
+              gust_mph,
+              uv,
+              condition: { text: conditionText, icon },
+            },
+            forecast: { forecastday }, // Destructuring forecastday directly
+          } = weatherData;
 
         store={
             ...store,
             name,
+            country,
+            localtime,
             conditionText,
+            icon,
             last_updated,
             last_updated_epoch,
             cloud,
@@ -133,11 +133,78 @@ async function setGlobalKey() {
             vis_miles,
             wind_kph,
             wind_mph,
-            uv
-        }
+            uv,forecastday
+        };
+        renderCurrentForecast();
+        renderFutureForecast();
+        changeBackgroundColor(is_day);
+
     } catch (error) {
         console.error('Error fetching weather data:', error);
     }
-})();
+}
 
 
+const currentMarkup = () =>{
+    return `
+    <div>
+  
+     <h1  id="location">${store.name}</h1>
+    <h2 class="info"> ${store.country}</h2>
+    <div class="weatherIcon">
+    ${store.is_day === 1 ? '<img class="icon" src="./images/cloud.png" alt="./images/sun.png">' : '<img class="icon" src="./images/shooting-star.png" alt="./images/moon.png">'}
+    </div>
+    <p class="conditions">${store.conditionText}</p> 
+    <p class="temp">${store.temp_c}</p>
+    <p class="feelslike">Real Feel ${store.feelslike_c}</p>
+    <div class="additional">
+        <p id="">Wind ${store.wind_kph} km/h </p>
+        <p id="">Cloud Cover ${store.cloud}%</p>
+        <p id="">Humidity ${store.humidity} %</p>
+        <p id="">Visibility ${store.vis_km} km</p>
+    </div>
+    `
+}
+const futureMarkup = () =>{
+    return  store.forecastday.map(day => {
+        return `
+        <div class="wrapper" >
+        <div class="container">
+            <h3 class="day">${day.date}</h3>
+            <div class="weatherIcon">
+                <img class="icon" src="${day.day.condition.icon}" alt="${day.day.condition.text}">
+                <div class="${day.day.condition.text}">
+                    <div class="inner"></div>
+                </div>
+            </div>
+            <p class="conditions">${day.day.condition.text}</p>
+            <p class="tempRange"><span class="high">${day.day.maxtemp_c}</span> | <span class="low">${day.day.mintemp_c}</span></p>
+        </div>
+        </div>
+        `
+    }).join('')
+}
+
+
+const renderCurrentForecast =  () => {
+    currentForcast.innerHTML = currentMarkup();
+}
+const renderFutureForecast =  () => {
+    futureForecast.innerHTML = futureMarkup();
+}
+    fetchWeatherData();
+
+});
+
+// Body background color based on is_day value 1 for no & 0 for yes
+const changeBackgroundColor = (is_day) => {
+    const body = document.body;
+    const isDay = is_day === 1;
+
+    if (isDay) {
+      body.style.backgroundColor = '#001247'; 
+    } else {
+      body.style.backgroundColor = '#003366'; 
+    }
+  };
+  
