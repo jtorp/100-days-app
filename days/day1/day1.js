faaee11110be89083ebaf744a320e5f14a75b768
Christian Torp <torp@Christians-Air.home>
2023-11-24 00:01:30 +0100

    updated handle input

--- days/day1/day1.js
+++ days/day1/day1.js
@@ -1,210 +1,248 @@
-document.addEventListener('DOMContentLoaded', function() { 
-// ---------------
-// Misc Functions
-// ---------------
+document.addEventListener('DOMContentLoaded', function () {
+    // ---------------
+    // Misc Functions
+    // ---------------
 
-const copyrightDate = document.getElementById('copyrightDate');
-const currentYear = new Date().getFullYear();
-copyrightDate.innerText = currentYear;
+    const copyrightDate = document.getElementById('copyrightDate');
+    const currentYear = new Date().getFullYear();
+    copyrightDate.innerText = currentYear;
 
-const location = document.getElementById('location');
-let geoCoordinates = null;
-const town = document.getElementById('town');
+    const location = document.getElementById('location');
+    const form = document.getElementById('form');
+    const textInput = document.getElementById('town');
+    let geoCoordinates = null;
 
+    // Geolocation functions
+    function getGeoLocation() {
+        store.name = 'Locating...';
+        async function geoLocationSuccess(position) {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
 
-// let lat =55.765478;
-// let lon= 12.500029
-// let q=`${lat},${lon}`;
+            try {
+                const osm = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lon}`;
+                const response = await fetch(osm);
+                if (!response.ok) {
+                    throw new Error('Error fetching data');
+                }
 
+                const data = await response.json();
+                console.log('Coordinates:', data.lat, data.lon);
+                store.name = data.address?.town + ', ' + data.address?.country || 'Unknown';
+            } catch (error) {
+                console.error('Error fetching town information:', error);
 
-// Geolocation functions
-function getGeoLocation() {
-    location.innerText = 'Locating...';
-
-async function geoLocationSuccess(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;     
-
-        try {
-            const osm = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lon}`;
-            const response = await fetch(osm);
-            if(!response.ok) {
-                throw new Error('Error fetching data');
             }
-
-            const data = await response.json();
-            console.log('Coordinates:', data.lat, data.lon);
-            location.innerText = data.address?.town + ', ' + data.address?.country || 'Unknown';
-        } catch (error) {
-            console.error('Error fetching town information:', error);
-
         }
-}
 
-function geoLocationError() {
-    location.innerText = 'Unable to retrieve your location';
-}
-    
-if (!navigator.geolocation) {
-    location.innerText = 'Geolocation is not supported by your browser';
-} else {
-    location.innerText = 'Locating...';
-    navigator.geolocation.getCurrentPosition(geoLocationSuccess, geoLocationError);
-}
-}
-    
-document.querySelector("#geoLocationBtn").addEventListener("click", getGeoLocation);
+        function geoLocationError() {
+            store.name.innerText = 'Unable to retrieve your location';
+        }
 
-// ---------------
-// Weather API https://www.weatherapi.com/
-// ---------------
-const currentForcast = document.getElementById('forecast');
-const futureForecast = document.getElementById('future');
+        if (!navigator.geolocation) {
+            location.innerText = 'Geolocation is not supported by your browser';
+        } else {
+            store.name = 'Locating...';
+            navigator.geolocation.getCurrentPosition(geoLocationSuccess, geoLocationError);
+        }
+    }
 
-let store = {
-        name: 'Copenhagen',
+    document.querySelector("#geoLocationBtn")?.addEventListener("click", getGeoLocation);
+
+    // ---------------
+    // Weather API https://www.weatherapi.com/
+    // ---------------
+    const currentForcast = document.getElementById('forecast');
+    const futureForecast = document.getElementById('future');
+
+    let store = {
+        name: 'Paris',
         last_updated: "",
         last_updated_epoch: 0,
-        cloud:0,
+        localtime: "",
         conditionText: '',
-        icon:'',
-        feelslike_c:0,
-        feelslike_f:0,
-        is_day:1,
-        temp_c:0,
-        temp_f:0,
-        humidity:0,
-        vis_km:0,
-        vis_miles:0,
-        wind_kph:0, 
-        wind_mph:0,
-        uv:null //Measure of the strength of the ultraviolet radiation from the sun. May be NULL.
-};
+        icon: '',
+        feelslike_c: 0,
+        is_day: 1,
+        temp_c: 0,
+        properties: {
+            cloud: {},
+            uv: {},
+            vis_km: {},
+            humidity: {},
+            wind_kph : {},
+        },
+        forecastday: [], 
+    };
 
 
-const fetchWeatherData = async () => {
-    try {
-        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=87a5d91b5b6d49aeb31181805232011&q=${store.name}&days=3&aqi=no&alerts=no
+    const fetchWeatherData = async () => {
+        try {
+            const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=87a5d91b5b6d49aeb31181805232011&q=${store.name}&days=3&aqi=no&alerts=no
         `);
-        const weatherData = await response.json();
-        console.log(weatherData)
+            const weatherData = await response.json();
 
-        const {
-            location: { name, country, localtime },
-            last_updated,
-            last_updated_epoch,
-            current: {
-              cloud,
-              feelslike_c,
-              feelslike_f,
-              is_day,
-              temp_c,
-              temp_f,
-              humidity,
-              vis_km,
-              vis_miles,
-              wind_kph,
-              gust_kph,
-              wind_mph,
-              gust_mph,
-              uv,
-              condition: { text: conditionText, icon },
-            },
-            forecast: { forecastday }, // Destructuring forecastday directly
-          } = weatherData;
+            const {
+                location: { name, country, localtime },
+                current: {
+                    last_updated,
+                    cloud,
+                    feelslike_c,
+                    is_day,
+                    temp_c,
+                    humidity,
+                    vis_km,
+                    wind_kph,
+                    uv,
+                    condition: { text: conditionText, icon },
+                },
+                forecast: { forecastday }, 
+            } = weatherData;
 
-        store={
-            ...store,
-            name,
-            country,
-            localtime,
-            conditionText,
-            icon,
-            last_updated,
-            last_updated_epoch,
-            cloud,
-            feelslike_c,
-            feelslike_f,
-            is_day,
-            temp_c,
-            temp_f,
-            humidity,
-            vis_km,
-            vis_miles,
-            wind_kph,
-            wind_mph,
-            uv,forecastday
-        };
-        renderCurrentForecast();
-        renderFutureForecast();
-        changeBackgroundColor(is_day);
+            store = {
+                ...store,
+                name,
+                country,
+                localtime,
+                conditionText,
+                icon,
+                last_updated,
+                feelslike_c,
+                is_day,
+                temp_c,
+                    properties: {
+                        cloud: {
+                            icon:`cloud`,
+                            value:`${cloud}%`
+                        },
+                        uv: {
+                            icon: "sunny",
+                            value:`UV Index:${uv}`
+                        },
+                        vis_km: {
+                            icon: "visibility",
+                            value:`${vis_km} km`
+                        },
+                        humidity: {
+                            icon: "humidity_percentage",
+                            value:`${humidity}%`
+                        },
+                        wind_kph : {
+                            icon: "air",
+                            value:`${wind_kph} km/h`
+                        }
+                    },             
+                forecastday
+            };
+            renderCurrentForecast();
+            renderFutureForecast();
+            changeBackgroundColor(is_day);
+            document.getElementById('lastUpdated').innerHTML = `Last Updated: ${last_updated}`
 
-    } catch (error) {
-        console.error('Error fetching weather data:', error);
+        } catch (error) {
+            currentForcast.innerHTML = `Error connecting to weather data, please try again later`;
+            console.error('Error fetching weather data:', error);
+
+        }
     }
-}
 
 
-const currentMarkup = () =>{
-    return `
-    <div>
-  
-     <h1  id="location">${store.name}</h1>
+    const currentMarkup = () => {
+        return `
+     <h1 id="location">${store.name}</h1>
     <h2 class="info"> ${store.country}</h2>
+    <p> Local time: ${store.localtime} </p>
     <div class="weatherIcon">
-    ${store.is_day === 1 ? '<img class="icon" src="./images/cloud.png" alt="./images/sun.png">' : '<img class="icon" src="./images/shooting-star.png" alt="./images/moon.png">'}
+  <img class="icon" src=${store.icon} alt=${store.conditionText}/>
     </div>
     <p class="conditions">${store.conditionText}</p> 
     <p class="temp">${store.temp_c}</p>
     <p class="feelslike">Real Feel ${store.feelslike_c}</p>
-    <div class="additional">
-        <p id="">Wind ${store.wind_kph} km/h </p>
-        <p id="">Cloud Cover ${store.cloud}%</p>
-        <p id="">Humidity ${store.humidity} %</p>
-        <p id="">Visibility ${store.vis_km} km</p>
-    </div>
+    <div class="properties">${currentProperties(store.properties)}</div>
     `
 }
-const futureMarkup = () =>{
-    return  store.forecastday.map(day => {
-        return `
-        <div class="wrapper" >
+
+    const currentProperties = (properties) => {
+ return Object.values(properties).map(({icon, value}) => {
+    return  `<p class=''> <span class="material-symbols-outlined">${icon}</span>
+    ${value}
+    </p>
+    `
+}).join('') }
+
+    const futureMarkup = () => {
+    const { forecastday } = store || { forecastday: [] }; 
+    return forecastday?.map(day => `
         <div class="container">
             <h3 class="day">${day.date}</h3>
             <div class="weatherIcon">
                 <img class="icon" src="${day.day.condition.icon}" alt="${day.day.condition.text}">
-                <div class="${day.day.condition.text}">
-                    <div class="inner"></div>
-                </div>
             </div>
             <p class="conditions">${day.day.condition.text}</p>
-            <p class="tempRange"><span class="high">${day.day.maxtemp_c}</span> | <span class="low">${day.day.mintemp_c}</span></p>
+            <p class="tempRange"><span class="high">High: ${day.day.maxtemp_c}</span> | <span class="high"> Low: ${day.day.mintemp_c}</span></p>
         </div>
-        </div>
-        `
-    }).join('')
+    `).join('');
 }
 
 
-const renderCurrentForecast =  () => {
-    currentForcast.innerHTML = currentMarkup();
-}
-const renderFutureForecast =  () => {
-    futureForecast.innerHTML = futureMarkup();
-}
+    const renderCurrentForecast = () => {
+        currentForcast.innerHTML = currentMarkup();
+    }
+    const renderFutureForecast = () => {
+        futureForecast.innerHTML = futureMarkup();
+    }
     fetchWeatherData();
 
-});
+
 
 // Body background color based on is_day value 1 for no & 0 for yes
 const changeBackgroundColor = (is_day) => {
     const body = document.body;
-    const isDay = is_day === 1;
-
-    if (isDay) {
-      body.style.backgroundColor = '#001247'; 
+    if (is_day === 1) {
+        body.style.backgroundColor = '#3a9efd';
+        body.style.color = '#ffffff';
     } else {
-      body.style.backgroundColor = '#003366'; 
+        body.style.backgroundColor = '#1a1b4b';
+        body.style.color = '#f5f5f5';
+        
     }
-  };
-  
+};
+// ---------------
+// Handle user input
+// ---------------
+
+const handleInput = (e) => {
+    store={
+        ...store,
+        name: e.target.value
+    }
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!store.name || !store.name.trim()) {
+       return
+    }
+    fetchWeatherData();
+}
+form.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter' || event.keyCode === 13) {
+        event.preventDefault(); // Prevent default form submission behavior
+        handleSubmit(event); // Call your form submission function
+    }
+});
+textInput.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
+
+
+
+
+
+
+
+
+
+
+
+
+});
